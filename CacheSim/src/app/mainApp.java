ed7d78f773c2f499967bd069633abd370ea432f4
fanqi1909 <fanqinus@gmail.com>
2015-10-29 14:44:48 +0800

    Setup
    Initial Setup

---
+++ CacheSim/src/app/mainApp.java
@@ -0,0 +1,16 @@
+package app;
+
+/**
+ * entry point of the entire simulator
+ * @author a0048267
+ *
+ */
+public class mainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
